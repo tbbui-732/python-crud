d747ccc040ec5a1aaf7dca852c96160ab28091d3
thienbaobui <80117447+tbbui-732@users.noreply.github.com>
2023-11-18 12:52:53 -0500

    feat: Comment out template code

--- main.py
+++ main.py
@@ -26,17 +26,19 @@ if __name__ == '__main__':
 
     with connection:
         with connection.cursor() as cursor:
+            pass
             # Create a new record
-            sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-            cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+            # sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+            # cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
 
         # connection is not autocommit by default. So you must commit to save
         # your changes.
         connection.commit()
 
         with connection.cursor() as cursor:
+            pass
             # Read a single record
-            sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-            cursor.execute(sql, ('webmaster@python.org',))
-            result = cursor.fetchone()
-            print(result)
+            # sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+            # cursor.execute(sql, ('webmaster@python.org',))
+            # result = cursor.fetchone()
+            # print(result)
